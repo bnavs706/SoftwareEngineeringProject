9adf016c2ba879d592fa64769ef663e3e806d114
bnavs706 <brandon_nava_@outlook.com>
2026-09-03 16:55:30 -0400

    Update Badcode.java
    making it say meow

--- src/project/annotations/Badcode.java
+++ src/project/annotations/Badcode.java
@@ -5,6 +5,6 @@ package project.annotations;
 public class Badcode {
 
     public static void main(String[] args) {
-        System.out.println("Testing");
+        System.out.println("Meow");
     }
 }
