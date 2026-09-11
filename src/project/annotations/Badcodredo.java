672cf99b77a7dcc85d91e4a9a3d1af7a9dd79bae
bnavs706 <brandon_nava_@outlook.com>
2026-09-10 23:04:25 -0400

    Update Badcodredo.java

--- src/project/annotations/Badcodredo.java
+++ src/project/annotations/Badcodredo.java
@@ -2,6 +2,6 @@ package project.annotations;
 
 public class Badcodredo {
      public static void main(String[] args) {
-        System.out.println("Meow")
+        System.out.println("Meow");
     }
 }
